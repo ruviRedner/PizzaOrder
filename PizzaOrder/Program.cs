23b8e00579dd70fbb4e504bb55e7559a2898d425
ruviRedner <r.y.r7966@gmail.com>
2024-08-11 09:07:47 +0300

    Add project files.

---
+++ PizzaOrder/Program.cs
@@ -0,0 +1,63 @@
+﻿
+
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+
+        NpolyonPizza();
+        alcsander();
+        Avrahm();
+       
+
+        Thread.Sleep(180000);
+
+    }
+
+    static async Task NpolyonPizza()
+    {
+        string pizza = "napolyon";
+        for (int i = 60; i > 0; i -= 10)
+        {
+            Console.WriteLine($"the pizza : {pizza} will be ready in {i} seconds ");
+            await Task.Delay(10000);
+           
+        }
+        Console.WriteLine(" npolyon is ready");
+
+       
+
+        
+
+    }
+
+    static async Task alcsander()
+    {
+
+        string pizzaa = "Alcsander";
+        for (int i = 120; i > 0; i-= 10)
+        {
+            Console.WriteLine($"the pizza : {pizzaa} will be ready in {i} seconds ");
+            await Task.Delay(10000);
+        }
+        Console.WriteLine(" alecsander is ready");
+
+
+
+    }
+
+    static async Task Avrahm()
+    {
+        
+        string pizzaaa = "Avrham";
+        for (int i = 180; i > 0; i -= 10)
+        {
+            Console.WriteLine($"the pizza : {pizzaaa} will be ready in {i} seconds ");
+            await Task.Delay(10000);
+        }
+        Console.WriteLine("avraham is ready");
+
+
+    }
+}
